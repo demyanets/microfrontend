db7536cc7adf2acd3edcc4d5b0be737e4df4d3c1
Taras Demyanets <mail4github@gmail.com>
2021-06-30 15:44:03 +0200

    Logging infrastructure was added

--- projects/common/src/lib/console-facade.ts
+++ projects/common/src/lib/console-facade.ts
@@ -1,10 +1,14 @@
 /* tslint:disable no-any */
+import { Level } from './level.enum';
+import { LoggerUtilities } from './logger-utilities';
 import { IConsoleFacade } from './console-facade-interface';
 
 /**
  * Encapsulates ConsoleAPI handling
  */
 export class ConsoleFacade implements IConsoleFacade {
+    public constructor(private readonly logLevel: Level = Level.INFO, private readonly appName: string = '') {}
+
     /**
      * Log error
      */
@@ -43,5 +47,55 @@ export class ConsoleFacade implements IConsoleFacade {
     log(message: string, ...optionalParams: any[]): void {
         // tslint:disable no-console
         console.log.apply(console, [message, ...optionalParams]);
+    }
+
+    private _logModern(level: Level, metaString: string, message: string, additional: any[]): void {
+        const color = LoggerUtilities.getColor(level);
+
+        // make sure additional isn't null or undefined so that ...additional doesn't error
+        additional = additional || [];
+
+        switch (level) {
+            case Level.WARNING:
+                console.warn(`%c${metaString}`, `color:${color}`, message, ...additional);
+                break;
+            case Level.ERROR:
+                console.error(`%c${metaString}`, `color:${color}`, message, ...additional);
+                break;
+            case Level.INFO:
+                console.info(`%c${metaString}`, `color:${color}`, message, ...additional);
+                break;
+            case Level.DEBUG:
+                console.debug(`%c${metaString}`, `color:${color}`, message, ...additional);
+                break;
+            default:
+                console.log(`%c${metaString}`, `color:${color}`, message, ...additional);
+        }
+    }
+
+    private _log(level: Level, message: string, additional: any[] = []): void {
+        const isLog2Console = this.logLevel >= level;
+        if (message && isLog2Console) {
+            const logLevelString = Level[level];
+
+            message = LoggerUtilities.prepareMessage(message);
+
+            const timestamp = new Date().toISOString();
+
+            const callerDetails = LoggerUtilities.getCallerDetails();
+
+            // if no message or the log level is less than the environ
+            if (isLog2Console) {
+                const metaString = LoggerUtilities.prepareMetaString(
+                    this.appName,
+                    timestamp,
+                    logLevelString,
+                    callerDetails.fileName,
+                    callerDetails.lineNumber
+                );
+
+                return this._logModern(level, metaString, message, additional);
+            }
+        }
     }
 }
